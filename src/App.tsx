4c2c8e971478f244a19e941bbc27aa5960823d74
fahimekhandan <fahimekhandan@gmail.com>
2023-09-28 14:36:43 +0330

    feat: created grids

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,24 @@
-function App(){
-  return <div>Hello World!</div>
+import { Grid, GridItem } from "@chakra-ui/react";
+
+function App() {
+  return (
+    <Grid
+      templateAreas={{
+        base: `"nav" "main"`,
+        lg: `"nav nav" "side main"`,
+      }}
+    >
+      <GridItem area={"nav"} bg="red">
+        Nav
+      </GridItem>
+      <GridItem area={"side"} bg="gold">
+        Side
+      </GridItem>
+      <GridItem area={"main"} bg="gray">
+        Main
+      </GridItem>
+    </Grid>
+  );
 }
 
-export default App
+export default App;
