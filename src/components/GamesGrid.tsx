7b98ecc687be5c19ce93b0494e68f5d26d2fc293
fahimekhandan <fahimekhandan@gmail.com>
2023-09-30 11:47:51 +0330

    feat: creat a custome hook to fetch the games

--- src/components/GamesGrid.tsx
+++ src/components/GamesGrid.tsx
@@ -1,42 +1,8 @@
-import { useEffect, useState } from "react";
-import apiClient from "../services/apiClient";
 import { Text } from "@chakra-ui/react";
-
-interface GamesType {
-  id: number;
-  slug: string;
-  name: string;
-  released: string;
-  tba: boolean;
-  background_image: string;
-  rating: number;
-  rating_top: number;
-  ratings_count: number;
-  reviews_text_count: string;
-  added: number;
-  metacritic: number;
-  playtime: number;
-  suggestions_count: number;
-  updated: string;
-}
-
-interface FetchedGamesResponse {
-  count: number;
-  results: GamesType[];
-}
+import useGame from "../hooks/useGame";
 
 const GamesGrid = () => {
-  const [games, setGames] = useState<GamesType[]>([]);
-  const [error, setError] = useState();
-
-  useEffect(() => {
-    apiClient
-      .get<FetchedGamesResponse>("/games")
-      .then(({ data }) => setGames(data.results))
-      .catch((err) => {
-        setError(err.message);
-      });
-  }, []);
+  const { games, error } = useGame();
 
   return (
     <div>
---
+++ src/hooks/useGame.tsx
@@ -0,0 +1,49 @@
+import { useEffect, useState } from "react";
+import apiClient from "../services/apiClient";
+import { CanceledError } from "axios";
+
+interface GamesType {
+  id: number;
+  slug: string;
+  name: string;
+  released: string;
+  tba: boolean;
+  background_image: string;
+  rating: number;
+  rating_top: number;
+  ratings_count: number;
+  reviews_text_count: string;
+  added: number;
+  metacritic: number;
+  playtime: number;
+  suggestions_count: number;
+  updated: string;
+}
+
+interface FetchedGamesResponse {
+  count: number;
+  results: GamesType[];
+}
+
+const useGame = () => {
+  const [games, setGames] = useState<GamesType[]>([]);
+  const [error, setError] = useState();
+
+  useEffect(() => {
+    const controller = new AbortController();
+
+    apiClient
+      .get<FetchedGamesResponse>("/games", { signal: controller.signal })
+      .then(({ data }) => setGames(data.results))
+      .catch((err) => {
+        if (err instanceof CanceledError) return;
+        setError(err.message);
+      });
+
+    return () => controller.abort();
+  }, []);
+
+  return { games, error };
+};
+
+export default useGame;
